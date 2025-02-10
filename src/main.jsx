70087520069e6e9755fa9fdf2b3003371a4d3216
alexborda <alexander.borda@gmail.com>
2025-02-10 12:22:11 -0400

    Detección de dispositivo (PC/Móvil) en UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { render } from 'preact'
 import './index.css'
-
+import './app.css'; // Asegura que el path sea correcto
 import { App } from './app.jsx'
 
 render(<App />, document.getElementById('app'))
