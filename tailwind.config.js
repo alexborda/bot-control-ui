2abca67136593bb3aea10b4667d0245c17735316
alexborda <alexander.borda@gmail.com>
2025-02-11 16:09:51 -0400

    optimiza odtener estado del bot

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 export default {
     darkMode: 'class', // Permite alternar entre modos claro y oscuro
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,css}"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
       extend: {},
     },
