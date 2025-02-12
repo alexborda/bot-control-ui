22f85826485b1aa896dc9890ee7332bd28c83984
alexborda <alexander.borda@gmail.com>
2025-02-12 07:29:31 -0400

    Fix serve dist path

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,11 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: "./", // Asegura rutas correctas en producción
+  base: "./",
   server: {
     port: 3000, // Usa 3000 por defecto si Railway no define un puerto
     host: true,
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name]-[hash].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]',
-      },
-    },
-  },
-  define: {
-    'process.env': {},
   },
 })
