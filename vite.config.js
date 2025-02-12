feefb1725db1c69839478bea1d962613b2bd184c
alexborda <alexander.borda@gmail.com>
2025-02-12 07:35:09 -0400

    Fix serve dist path

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import preact from '@preact/preset-vite'
 
 export default defineConfig({
   plugins: [preact()],
-  base: "./",
+  base: "./", // Evita problemas de rutas en producción
+  build: {
+    outDir: "dist", // Se asegura de que el build se guarde correctamente
+  },
   server: {
-    port: 3000, // Usa 3000 por defecto si Railway no define un puerto
+    port: 3000, // Usa un puerto estándar
     host: true,
   },
 })
