9cd1bb9e2dc19874b7e6697af48ac3aaccccf19f
alexborda <alexander.borda@gmail.com>
2025-02-12 09:18:25 -0400

    Fix PORT issue

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from 'vite';
+import preact from '@preact/preset-vite';
 
 export default defineConfig({
   plugins: [preact()],
-  base: "./", // Evita problemas de rutas en producción
-  build: {
-    outDir: "dist", // Se asegura de que el build se guarde correctamente
-  },
+  base: "./",  // Asegura rutas correctas en producción
   server: {
-    host: true,
-  },
-})
+    host: "0.0.0.0",
+    port: parseInt(process.env.PORT) || 8080
+  }
+});
