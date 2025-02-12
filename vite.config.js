e24e71fde1e6c72e5988a98cfd52a2a75eb1815b
alexborda <alexander.borda@gmail.com>
2025-02-12 11:11:42 -0400

    Fix: usar correctamente el puerto en Railway

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,6 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: process.env.PORT || 8080, // Usa el puerto de Railway o 3000 por defecto
+    port: 8080, // Usa el puerto de Railway o 3000 por defecto
   },
 });
