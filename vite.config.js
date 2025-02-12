307eb2248e90105d5cc9c98eadb1ca923217d34b
alexborda <alexander.borda@gmail.com>
2025-02-12 09:43:57 -0400

    Fix: usar correctamente el puerto en Railway

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import preact from '@preact/preset-vite';
 export default defineConfig({
   plugins: [preact()],
   base: "./", // Evita problemas de rutas en producción
-  build: {
-    outDir: "dist", // Asegura que la build se guarde en "dist"
-  },
   server: {
     port: process.env.PORT || 3000, // Usa Railway si está disponible
     host: true,
